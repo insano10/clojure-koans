a329907bff8feaecbbe2b795b39e97a968b63a2a
insano10 <jdommett@gmail.com>
2018-07-08 11:07:53 +0100

    solve 14

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,41 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         acc  ()]
+    (if (seq coll)
+      (recur (rest coll) (conj acc (first coll)))
+      acc
+    )
+  ))
 
 (defn factorial [n]
-  __)
+  (if (= 1 n)
+    n
+    (* n (factorial (dec n)))
+  )  
+)
+
+(defn factorial2 [n]
+  (loop [num n
+         acc 1]
+    (if (= 0 num)
+      acc
+      (recur (dec num) (* num acc))
+    )
+  )
+)
 
 (meditations
   "Recursion ends with a base case"
@@ -51,4 +71,4 @@
   (< 1000000000000000000000000N (factorial 1000N))
 
   "But what happens when the machine limits you?"
-  (< 1000000000000000000000000N (factorial 100003N)))
+  (< 1000000000000000000000000N (factorial2 100003N)))
